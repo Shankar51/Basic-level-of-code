3d8f5b879b454bc2f5f007122e068d3425f391a3
Shankar51 <136904882+Shankar51@users.noreply.github.com>
2024-03-06 15:06:22 +0530

    first commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter the number till u want fibonacci");
+		
+
+	}
+
+}

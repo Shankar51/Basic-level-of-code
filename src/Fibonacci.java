ad8a83c1744af1a5d8b2c993a23c6b3935369dfb
Shankar51 <136904882+Shankar51@users.noreply.github.com>
2024-03-06 15:40:22 +0530

    added one more program fibonacci series

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		System.out.println("enter the number till u want fibonacci");
-		
+		int n = scan.nextInt();
+		int a=0;
+		int b=1;
+		int c;
+		for(int i=0;i<n;i++) {
+			c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+			
+		}
 
 	}
 
